d224fa34bdf64f39f05f7178599890307da2b42c
Ignacio Filippi <ifilippi@zetech.com.ar>
2019-06-07 09:00:59 -0300

    cambios en general

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
             <app-formulario v-on:recibirDatos="onChildClick"></app-formulario>
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <div class="col-xs-4 col-md-4 col-lg-4 col-sm-4">
-                    <app-euler v-model="dataSwitch"></app-euler>
+                    <app-euler v-model="dataSwitch" :parentData="myData"></app-euler>
                 </div>
                 <div class="col-xs-4 col-md-4 col-lg-4 col-sm-4">
                     <app-euler-mejorado v-model="dataSwitch"></app-euler-mejorado>
@@ -12,6 +12,7 @@
                 <div class="col-xs-4 col-md-4 col-lg-4 col-sm-4">
                     <app-runge v-model="dataSwitch"></app-runge>
                 </div>
+                <button @click="showData">DAT</button>
             </div>
         </div>
     
@@ -21,18 +22,29 @@
 import Euler from './components/graficos/Euler.vue';
 import EulerMejorado from './components/graficos/EulerMejorado.vue';    
 import RungeKutta from './components/graficos/RungeKutta.vue';
-import Formulario from './components/Formulario.vue';    
+import Formulario from './components/Formulario.vue';
 
 export default {
     data(){
         return {
-            funcion: '',
-            t0:'',
-            x0:'',
-            n:'',
-            h:'',
-            b:'',
-            intervalo:''
+            myData:{
+                f: '',
+                x0:'',
+                y0:'',
+                n:'',
+                h:'',
+                b:'',
+                intervalo:''
+            }
+            // posts: [
+            //     { id: 1, f: '' },
+            //     { id: 2, x0: '' },
+            //     { id: 3, y0: '' },
+            //     { id: 4, n: '' },
+            //     { id: 5, h: '' },
+            //     { id: 6, b: '' },
+            //     { id: 7, intervalo: '' },
+            // ]
         }
     },
     components: {
@@ -41,16 +53,16 @@ export default {
         appEulerMejorado: EulerMejorado,
         appFormulario: Formulario,
     },
-    methods:{
-        onChildClick (funcion,t0,x0,n,h,b,intervalo) {
-            this.funcion = funcion,
-            this.t0 = t0,
-            this.x0 = x0,
-            this.n = n,
-            this.h = h,
-            this.b = b,
-            this.intervalo = intervalo
+    methods: {
+        onChildClick(myData) {
+            console.log(myData);
+            this.myData = myData;
+        },
+        showData(){
+            console.log('data: ', this.myData);
+            
         }
+
     }
     
 }
